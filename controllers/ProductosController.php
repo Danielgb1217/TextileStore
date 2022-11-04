97a6421af2298784688ad36dc9c0482ca895777f
Danielgb1217 <Danielgb1217@gmail.com>
2022-11-04 16:40:35 -0500

    Correccicon actualizacion de pedido

--- controllers/ProductosController.php
+++ controllers/ProductosController.php
@@ -196,7 +196,7 @@ class ProductosController{
         
 
         if($_SERVER['REQUEST_METHOD'] === 'POST'){           
-             echo($_POST);
+             
 
              if(($_POST['opcion'])?? "" === 'buscar'){ 
 

d69a36aabbe853328fddadcdf1858bd117238051
Danielgb1217 <Danielgb1217@gmail.com>
2022-11-03 21:13:36 -0500

    Css Responsive

--- controllers/LoginController.php
+++ controllers/LoginController.php
@@ -121,11 +121,11 @@ class LoginController{
                     $password = $_POST['password'];
                     
                     $alertas =  $password->validarPassword();
-    
+                    debuguear($alertas);
                     //UNA COSA ES MI USUARIO ESPEJ DE LA BASE DE DATOS-->$usuario<-- Y OTRA ES EL USUARIO CREADO DEL FORMULARIO POST -->$password<--
                     if(empty($alertas)){    //Si pasamos la validadcion el arreglo de alertas estara vacio y podemos hashear el password
                         $usuario->password = ' ';  //borro el password viejo
-                        debuguear($usuario);
+                        
                         $usuario->password = $password;     //sobreescribo el passwor que e traigo por el metodo post del frm
                         
                         //$usuario->hashPassword();
